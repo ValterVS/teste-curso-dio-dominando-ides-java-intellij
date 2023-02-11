b68200f8c8b737101aecdcd64e11b7f1822ab7ab
Valter Silva <valter.silva.eng@gmail.com>
2023-02-11 16:55:28 -0300

    firts commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+       /* int a = 5;
+        int b = 3;
+        System.out.println("Hello World!" + (a+b));
+        System.out.println("Hello World!");*/
+        Gato gato = new Gato();
+    }
+}
